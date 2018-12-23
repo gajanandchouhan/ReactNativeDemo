75a7125bf0dfacb21b7383f7de82a909cd5b9c97
hp <gajanand.chouhan1991@gmail.com>
2018-12-23 21:33:24 +0530

    flat list

--- component/Home.js
+++ component/Home.js
@@ -1,18 +1,64 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text,Alert,ActivityIndicator,FlatList, TouchableOpacity } from 'react-native';
 
 export default class Home extends Component {
+
   constructor(props) {
     super(props);
     this.state = {
+      isLoading:true
     };
   }
-
+  _renderItem = ({item}) => (
+    <ListItem
+      onPressItem={this._onPressItem}
+      title={item.title}
+    />
+  );
+componentDidMount(){
+  return fetch('https://facebook.github.io/react-native/movies.json').then((response)=> response.json())
+  .then((responseJson)=>{
+    this.setState({
+      isLoading:false,
+      dataSource:responseJson.movies
+    })
+  }).catch((error)=>{
+    this.setState({
+      isLoading:false
+    })
+  })
+}
   render() {
+    if(this.state.isLoading){
+      return (
+        <View style={{flex: 1, padding: 20}}>
+        <ActivityIndicator />
+      </View>
+      );
+    }
     return (
       <View>
-        <Text> Home screen navigation </Text>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={this._renderItem}
+          keyExtractor={({id}, index) => id}
+        />
       </View>
+    );
+  }
+}
+
+class ListItem extends React.PureComponent{
+  render(){
+    return (
+      <TouchableOpacity onPress={this._onPress}>
+        <View>
+          <Text style={{paddingTop:10, paddingBottom:10, marginLeft:10}}>
+            {this.props.title}
+          </Text>
+          <View style={{height:1, backgroundColor:'gray'}} />
+        </View>
+      </TouchableOpacity>
     );
   }
 }
--- component/LandingScreen.js
+++ component/LandingScreen.js
@@ -11,16 +11,10 @@ const DrawerStack=createDrawerNavigator({
 
 const DrawerContainer=createAppContainer(DrawerStack);
 export default class LandingScreen extends Component {
-
-    static navigationOptions = {
-        drawerLabel: 'Home',
-        drawerIcon: ({ tintColor }) => (
-          <Image
-            source={require('./drawers.png')}
-            style={[styles.icon, {tintColor: tintColor}]}
-          />
-        ),
-      };
+  static navigationOptions = {
+  headerLeft:<Image source={require('../img/drawers.png')} style={{width:30, height:30, marginLeft:10}} onPress={()=>this.props.navigation.toggleDrawer()}/>
+  };
+   
   render(){
       return <DrawerContainer />;
   }
@@ -32,3 +26,35 @@ const styles = StyleSheet.create({
       height: 24,
     },
   });
+  class HamburgerIcon extends Component {
+
+    toggleDrawer = () => {
+  
+      console.log(this.props.navigationProps);
+  
+      this.props.navigationProps.toggleDrawer();
+  
+    }
+  
+    render() {
+  
+      return (
+  
+        <View style={{ flexDirection: 'row' }}>
+  
+          <TouchableOpacity onPress={this.toggleDrawer.bind(this)} >
+  
+            <Image
+              source={{ uri: 'https://reactnativecode.com/wp-content/uploads/2018/04/hamburger_icon.png' }}
+              style={{ width: 25, height: 25, marginLeft: 5 }}
+            />
+  
+          </TouchableOpacity>
+  
+        </View>
+  
+      );
+  
+  
+    }
+  }
